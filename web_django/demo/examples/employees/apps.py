d30f5b5313fefc3df6515ad6171b0cad5ddc9b43
prabhu2308 <prabhu2308@gmail.com>
2017-11-01 18:06:40 +0530

    Django working template.

---
+++ web_django/demo/examples/employees/apps.py
@@ -0,0 +1,22 @@
+from django.apps import AppConfig
+from django.utils.translation import ugettext_lazy as _
+
+from material.frontend.apps import ModuleMixin
+
+
+class EmployeesConfig(ModuleMixin, AppConfig):
+    name = 'demo.examples.employees'
+    icon = '<i class="material-icons">people</i>'
+    verbose_name = _('Employees')
+
+    def has_perm(self, user):
+        return user.is_superuser
+
+class EmployeesAdminConfig(ModuleMixin, AppConfig):
+    name = 'demo.examples.employees'
+    icon = '<i class="material-icons">people</i>'
+    verbose_name = _('Admin Page')
+
+
+    def has_perm(self, user):
+        return user.is_superuser
